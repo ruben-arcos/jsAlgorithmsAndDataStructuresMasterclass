3e5d546fe7a00fc2b612dcb767f61bec8bce0894
Ruben Arcos <ruben.arcos06@gmail.com>
2023-12-13 23:27:09 -0600

    added second solution to exercise 1. used only loops

--- exercises/exerciseOne.js
+++ exercises/exerciseOne.js
@@ -39,11 +39,41 @@ function validAnagram(str1, str2) {
     return true
   }
 
-  console.log(validAnagram('', '')) // true
-  console.log(validAnagram('aaz', 'zza')) // false
-  console.log(validAnagram('anagram', 'nagaram')) // true
-  console.log(validAnagram("rat","car")) // false // false
-  console.log(validAnagram('awesome', 'awesom')) // false
-  console.log(validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana')) // false
-  console.log(validAnagram('qwerty', 'qeywrt')) // true
-  console.log( validAnagram('texttwisttime', 'timetwisttext')) // true
+console.log(validAnagram('', '')) // true
+console.log(validAnagram('aaz', 'zza')) // false
+console.log(validAnagram('anagram', 'nagaram')) // true
+console.log(validAnagram("rat","car")) // false // false
+console.log(validAnagram('awesome', 'awesom')) // false
+console.log(validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana')) // false
+console.log(validAnagram('qwerty', 'qeywrt')) // true
+console.log( validAnagram('texttwisttime', 'timetwisttext')) // true
+
+
+// different version 
+
+function validAnagram(first, second) {
+    if (first.length !== second.length) {
+        return false;
+    }
+
+    const lookup = {};
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        // if letter exists, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    
+    for (let i = 0; i < second.length; i++)  {
+        let letter = second[i];
+        // can't find letter or letter is zero then it's not an anagram
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+    return true
+}
+
+console.log(validAnagram('kayak', 'kayak')) // true
