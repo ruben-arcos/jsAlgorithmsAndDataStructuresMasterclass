8b7262b9cbf71ff4591a643037fdfe84959bb59d
Ruben Arcos <ruben.arcos06@gmail.com>
2023-12-29 00:34:48 -0600

    Added second solution to fibonacci exercise

--- exercises/exercise14_fibonacci.js
+++ exercises/exercise14_fibonacci.js
@@ -28,3 +28,19 @@ console.log(fib(10)); // 55
 console.log(fib(28)); // 317811
 console.log(fib(35)); // 9227465
 
+// Solution 2
+
+function fib2(num) {
+    // Base case: if num is less than or equal to 2, return 1
+    if (num <= 2) {
+        return 1
+    }
+    // Recursive step to calculate the next Fibonacci number
+    return fib(num - 1) + fib(num - 2)
+}
+
+console.log(fib2(5)); // 5
+console.log(fib2(11)); // 89
+console.log(fib2(29)); // 514229
+console.log(fib2(34)); // 5702887
+
