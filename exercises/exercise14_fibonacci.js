67d9d77fccac8b5d1c18165e4871fb9dbfff70bf
Ruben Arcos <ruben.arcos06@gmail.com>
2023-12-29 00:33:10 -0600

    Completed coding exercise 14: fibonacci

---
+++ exercises/exercise14_fibonacci.js
@@ -0,0 +1,30 @@
+/**
+    Fibonacci
+
+        Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+
+        Examples:
+          fib(4) // 3
+          fib(10) // 55
+          fib(28) // 317811
+          fib(35) // 9227465
+*/
+
+function fib(num) {
+    // Base case: if num is 0, return num
+  if (num === 0) {
+    return 0;
+  }
+   // Base case: if num is 1, return 1
+  if (num === 1) {
+    return 1
+  }
+// Calculate the next Fibonacci number by recursively adding the last two numbers
+  return fib(num - 1) + fib(num - 2)
+}
+
+console.log(fib(4)); // 3
+console.log(fib(10)); // 55
+console.log(fib(28)); // 317811
+console.log(fib(35)); // 9227465
+
