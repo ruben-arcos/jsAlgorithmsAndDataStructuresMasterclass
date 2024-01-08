c7aec432ae2cb248ca42a26b0e42f0083b14fa61
Ruben Arcos <ruben.arcos06@gmail.com>
2024-01-08 01:22:06 -0600

    added Bubble Sort: Otimization solution as well as comments on every solution

--- examples/bubbleSort.js
+++ examples/bubbleSort.js
@@ -25,17 +25,26 @@ console.log(bubbleSort([37,45,29,8]))
 
 // solution 2
 function bubbleSort2 (arr) {
+    // Temporary variable for swapping
     let temp;
 
+    // Outer loop - iterates from the end of the array towards the beginning
     for (let i = arr.length; i > 0; i--) {
+        // Inner loop - iterates from the beginning up to i (exclusive)
         for (let j = 0; j < i - 1; j++) {
+            // Compare adjacent elements in the array
             if (arr[j] > arr[j+1]) {
+                 // Store the value at arr[j] in the temporary variable temp
                 temp = arr[j]
+                // Assign the value at arr[j + 1] to arr[j]
                 arr[j] = arr[j+1]
+                // Assign the value stored in temp to arr[j + 1]
                 arr[j+1] = temp
+                // Swap the elements at indices j and j + 1
             }
         }
     }
+    // Return the sorted array
     return arr
 }
 
@@ -44,18 +53,54 @@ console.log(bubbleSort2([5,85, -10,0,6]))
 
 // ES2015 solution
 function bubbleSort3(arr) {
+    // Helper function to swap elements at given indices in the array
     const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]] 
     };
 
+    // Outer loop - iterates from the end of the array towards the beginning
     for ( let i = arr.length; i > -0; i-- ) {
+         // Inner loop - iterates from the beginning up to i (exclusive)
         for ( let j = 0; j < i - 1; j++ ) {
+            // Compare adjacent elements in the array
             if (arr[ j ] > arr[ j+1 ]) {
+                // If the current element is greater than the next one, perform a swap
                 swap( arr, j, j+1)
             }
         }
     }
+    // Return the sorted array
     return arr
 }
 
 console.log(bubbleSort3([8,12,29,-3,37,45,88]))
+
+// Bubble sort optimization with no swaps
+function bubbleSortOptimized(arr) {
+    // Variable to track whether any swaps occurred in the inner loop
+    let noSwaps;
+
+    // Outer loop - iterates from the end of the array towards the beginning
+    for (let i = arr.length; i > 0; i--) {
+        // Assume no swaps have occurred at the beginning of each pass
+        noSwaps = true;
+        // Inner loop - iterates from the beginning up to i (exclusive)
+        for (let j = 0; j < i - 1; j++) {
+            // Compare adjacent elements in the array
+            if(arr[j] > arr[j+ 1]) {
+                // If the current element is greater than the next one, perform a swap
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                // Since a swap occurred, set noSwaps to false
+                noSwaps = false;
+            }
+        }
+         // If no swaps occurred in the entire inner loop, the array is already sorted
+        if(noSwaps) break;
+    }
+    // Return the sorted array
+    return arr
+}
+
+console.log(bubbleSortOptimized([8,1,2,3,4,5,6,7]))
