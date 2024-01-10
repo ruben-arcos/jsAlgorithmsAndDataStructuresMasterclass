cc52fd70d2b1941420c56f78b7525103c3757b99
Ruben Arcos <ruben.arcos06@gmail.com>
2024-01-10 01:52:07 -0600

    Added recursive breakdown for leftSide and rightSide in mergeSort

--- examples/mergeSort.js
+++ examples/mergeSort.js
@@ -61,5 +61,44 @@ function mergeSort(array) {
 
 console.log(mergeSort([10, 24, 76, 72, 1, 9]));
 
+/**
+    Recursive breakdown for leftSide and rightSide
 
+    1. Initial Call:
+        1.1. mergeSort([10, 24, 76, 72, 1, 9])
+        1.2, Checks the base case: array.length > 1, so it continues with the recursive calls.
+        1.3. Calculates the midPoint: Math.floor(6 / 2), which is 3.
 
+        leftSide:
+        1.4. Calls mergeSort on the left side: mergeSort([10, 24, 76])
+            1.4.1. Checks the base case: array.length > 1, so it continues with the recursive calls.
+            1.4.2. Calculates the midPoint: Math.floor(3 / 2), which is 1.
+            1.4.3. Calls mergeSort on the left side: mergeSort([10])
+                    * Checks the base case: array.length <= 1 (base case reached), returns [10].
+            1.4.4. Calls mergeSort on the right side: mergeSort([24, 76])
+                    * Checks the base case: array.length > 1, so it continues with the recursive calls.
+                    * Calculates the midPoint: Math.floor(2 / 2), which is 1.
+                    * Calls mergeSort on the left side: mergeSort([24])
+                        * Checks the base case: array.length <= 1 (base case reached), returns [24].
+                    * Calls mergeSort on the right side: mergeSort([76])
+                        * Checks the base case: array.length <= 1 (base case reached), returns [76].
+                    * Merges [24] and [76] => [24, 76]
+            1.4.5. Merges [10] and [24, 76] => [10, 24, 76]
+
+        rightSide:
+        1.5. Calls mergeSort on the right side: mergeSort([72, 1, 9])
+        1.5.1. Checks the base case: array.length > 1, so it continues with the recursive calls.
+        1.5.2. Calculates the midPoint: Math.floor(3 / 2), which is 1.
+        1.5.3. Calls mergeSort on the left side: mergeSort([72])
+            * Checks the base case: array.length <= 1 (base case reached), returns [72].
+        1.5.4. Calls mergeSort on the right side: mergeSort([1, 9])
+            * Checks the base case: array.length > 1, so it continues with the recursive calls.
+            * Calculates the midPoint: Math.floor(2 / 2), which is 1.
+            * Calls mergeSort on the left side: mergeSort([1])
+                * Checks the base case: array.length <= 1 (base case reached), returns [1].
+            * Calls mergeSort on the right side: mergeSort([9])
+                * Checks the base case: array.length <= 1 (base case reached), returns [9].
+            * Merges [1] and [9] => [1, 9]
+        1.5.5. Merges [72] and [1, 9] => [1, 9, 72]
+     1.5.6. Merges [10, 24, 76] and [1, 9, 72] => [1, 9, 10, 24, 72, 76]
+*/
