f7ceb6092b39ac23bacb41554b27766107c5a20c
Ruben Arcos <ruben.arcos06@gmail.com>
2024-01-20 02:05:39 -0600

    Finished code example for shift()

---
+++ examples/section19_singlyLinkedList/04_shift.js
@@ -0,0 +1,101 @@
+/**
+    shift() → Removing a new NODE from the beginning of the Linked List
+    ● If there are no nodes, return undefined
+    ● Store the current head property in a variable
+    ● Set the head property to be the current head's next property
+    ● Decrement length by 1
+    ● Return the value of the node removed
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+
+    // if no head
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    // Check if the list is empty
+    if (!this.head) {
+      // If empty, return undefined (no node to remove)
+      return undefined;
+    } else {
+      // Initialize pointers for traversal
+      let currentNode = this.head;
+      let newTail = currentNode;
+
+      // Traverse the list until the last node is reached
+      while (currentNode.next) {
+        // Update newTail to the current node
+        newTail = currentNode;
+        // Move to the next node
+        currentNode = currentNode.next;
+      }
+
+      // Update tail to the second-to-last node
+      this.tail = newTail;
+      // Set the next reference of the new tail to null, effectively removing the last node
+      this.tail.next = null;
+      // Decrease the length of the list
+      this.length--;
+
+      // Check if the list is now empty
+      if (this.length === 0) {
+        // If empty, set both head and tail to null
+        this.head = null;
+        this.tail = null;
+      }
+      // Return the removed last node
+      return currentNode;
+    }
+  }
+  shift() {
+    // Check if the linked list is empty
+    if (!this.head) {
+      // If empty, return undefined as there is no node to remove
+      return undefined;
+    }
+    // Create a variable to store the current head of the linked list
+    let currentHead = this.head;
+    // Update the head to be the next node in the linked list
+    this.head = currentHead.next;
+    // Decrement the length of the linked list by 1
+    this.length--;
+
+    // Check if the length has become 0 after the removal
+    if (this.length === 0) {
+      // If the length is 0, set the tail to null as the linked list is now empty
+      this.tail = null;
+    }
+    // Return the removed node (the original head)
+    return currentHead;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push(80);
+list.push(85);
+list.push(90);
+
+console.log(list);
