f1aaf055388602b9e3fa1f7546db88f97637e3a6
Ruben Arcos <ruben.arcos06@gmail.com>
2024-01-20 22:54:22 -0600

    Finished coding example Singly Linked List: Push

---
+++ examples/SinglyLinkedList/02_push.js
@@ -0,0 +1,49 @@
+/**
+    push
+    ● This function should accept a value
+    ● Create a new node using the value 
+       passed to the function
+    ● If there is no head property on the 
+       list, set the head and tail to be the 
+       newly created node
+    ● Otherwise set the next property on 
+       the tail to be the new node and set 
+       the tail porperty on the lost to be the 
+       newly created node
+    ● Increment the length by one
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.val = null;
+    this.next = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val)
+
+    // if no head
+    if (!this.head) {
+        this.head = newNode;
+        this.tail= this.head
+    } else {
+        this.tail.next = newNode;
+        this.tail = newNode
+    }
+    this.length++
+  }
+}
+
+
+let list = new SinglyLinkedList();
+list.push(80);
+list.push(85)
+
+console.log(list)
