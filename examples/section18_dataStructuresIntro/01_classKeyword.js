d2692d8d81e0ed1f15166b25b3d4c79bc2676a3a
Ruben Arcos <ruben.arcos06@gmail.com>
2024-01-29 23:09:20 -0600

    Created folder and added coding examples for Section 18: Data Structures Introduction

---
+++ examples/section18_dataStructuresIntro/01_classKeyword.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor(firstName, lastName, year){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = year;
+    }
+}
+
+let firstStudent = new Student("Ruben", "Arcos",1);
+let secondStudent = new Student("Jack", "Wood",2);
+
+
